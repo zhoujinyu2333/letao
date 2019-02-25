d9bade1edc281ec3953480b5745ddda24c527361
zhoujinyu2333 <1006440100@qq.com>
2019-02-25 21:06:58 +0800

    公共功能部分

--- public/backs/js/common.js
+++ public/backs/js/common.js
@@ -1,13 +1,40 @@
+$(document).ajaxStart(function () {
+    NProgress.start();
+});
+
+$(document).ajaxStop(function () {
+    setTimeout(function () {
+        NProgress.done();
+    }, 1000)
+});
+
 $(function () {
 
-    $(document).ajaxStart(function () {
-        NProgress.start();
+    $(".lt_left .open").on("click", function () {
+        $(this).next().stop().slideToggle();
     });
 
-    $(document).ajaxStop(function () {
-        setTimeout(function () {
-            NProgress.done();
-        },1000)
+    $(".lt_header .open1").on("click", function () {
+        $(".lt_left").toggleClass("hides");
+        $(".lt_main").toggleClass("hides");
+        $(".lt_header").toggleClass("hides");
+    });
+
+    $(".lt_header .open2").on("click", function () {
+        $("#mymodal").modal("show");
+    });
+
+    $("#close").on("click", function () {
+        $.ajax({
+            type: "get",
+            url: "/employee/employeeLogout",
+            dataType: "json",
+            success: function (res) {
+                if (res.success) {
+                    location.href = "login.html";
+                }
+            }
+        })
     })
 
 })
---
+++ public/backs/js/isLogin.js
@@ -0,0 +1,10 @@
+$.ajax({
+    type: "get",
+    url: "/employee/checkRootLogin",
+    dataType: "json",
+    success: function (res) {
+        if (res.error === 400) {
+            location.href = "login.html";
+        }
+    }
+})
